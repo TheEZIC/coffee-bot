6e85dd3469deacaefa68835402b379a513a06de7
TheEZIC <35061249+TheEZIC@users.noreply.github.com>
2021-04-03 16:51:56 +0300

    Update Server.ts

--- src/Server.ts
+++ src/Server.ts
@@ -2,7 +2,7 @@ import express from "express";
 
 const Express = express();
 
-Express.all("./", (req, res) => res.send("ok"));
+Express.all("/", (req, res) => res.send("ok"));
 
 const startServer = () => Express.listen(3000, () => console.log("[Server] server started"));
 
